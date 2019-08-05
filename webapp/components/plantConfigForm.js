49cda559c28cb3a7afd63188c18b0e4dd4b637cf
Jonas Bakken <jonasxb@gmail.com>
2019-08-05 16:52:21 +0200

    Show status, use hooks

--- webapp/components/plantConfigForm.js
+++ webapp/components/plantConfigForm.js
@@ -1,45 +1,69 @@
+import { useState } from "react";
 import { Component } from "react";
-import { setConfig } from "utils/req";
+import { setConfig as postConfig } from "utils/req";
 
-class PlantConfigForm extends Component {
-  constructor(props) {
-    super(props);
-    const { plant, water } = props.config || {};
-    this.state = { plant, water };
-  }
-  render() {
-    const { id } = this.props;
-    return (
-      <div key={id}>
-        <div>Id: {id}</div>
-        <input
-          value={this.state["plant"] || ""}
-          onChange={input => {
-            const value = input.target.value;
-            this.setState({
-              plant: value !== "" ? value : undefined
-            });
-          }}
-        />
-        <input
-          value={this.state["water"] || ""}
-          onChange={input => {
-            const value = parseInt(input.target.value);
-            this.setState({
-              water: value || undefined
-            });
-          }}
-        />
-        <button
-          onClick={() =>
-            setConfig(id, this.state["plant"], this.state["water"])
-          }
-        >
-          Configure
-        </button>
-      </div>
-    );
-  }
-}
+const Input = ({ title, value, onChange }) => (
+  <div>
+    <div>{title}:</div>
+    <input
+      value={value || ""}
+      onChange={input => onChange(input.target.value)}
+    />
+  </div>
+);
+
+const Status = ({ active }) => (
+  <div>
+    <div
+      style={{
+        backgroundColor: active ? "green" : "red",
+        width: "1em",
+        height: "1em",
+        borderRadius: "0.5em",
+        display: "inline-block"
+      }}
+    />
+    {active ? "Online" : "Offline"}
+  </div>
+);
+
+const PlantConfigForm = ({ client, onPost }) => {
+  const { id, active, config: currentConfig } = client;
+  const [config, setConfig] = useState(currentConfig || {});
+  return (
+    <div>
+      <div>Id: {id}</div>
+      <Status active={active} />
+      <Input
+        title="Plant"
+        value={config.plant}
+        onChange={value =>
+          setConfig({
+            ...config,
+            plant: value !== "" ? value : undefined
+          })
+        }
+      />
+      <Input
+        title="Water"
+        value={config.water}
+        onChange={value =>
+          setConfig({
+            ...config,
+            water: parseInt(value) || undefined
+          })
+        }
+      />
+      <button
+        onClick={async () => {
+          const result = await postConfig(id, config.plant, config.water);
+          onPost(id, result.data.setConfig);
+        }}
+      >
+        Configure
+      </button>
+    </div>
+  );
+};
 
 export default PlantConfigForm;
--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,29 +1,44 @@
+import { useState } from "react";
 import Container from "components/container";
 import { getClients } from "utils/req";
 import Link from "next/link";
 import PlantConfigForm from "components/plantConfigForm";
 
-const Index = ({ clients }) => {
+const ClientList = ({ clients, title, onPost }) => (
+  <div>
+    <h2>{title}</h2>
+    {clients.map(client => (
+      <PlantConfigForm key={client.id} client={client} onPost={onPost} />
+    ))}
+  </div>
+);
+
+const Index = ({ clients: currentClients }) => {
+  const [clients, setClients] = useState(currentClients);
   const configured = clients.filter(c => !!c.config);
   const unconfigured = clients.filter(c => !c.config);
+  const onPost = (id, config) =>
+    setClients(
+      clients.map(client => (client.id === id ? { ...client, config } : client))
+    );
   return (
     <Container>
       <h2>IOH</h2>
       {unconfigured.length > 0 && (
-        <div>
-          <h2>Unconfigured</h2>
-          {unconfigured.map(client => (
-            <PlantConfigForm key={client.id} {...client} />
-          ))}
-        </div>
+        <ClientList
+          title="Unconfigured"
+          clients={unconfigured}
+          setClients={setClients}
+          onPost={onPost}
+        />
       )}
       {configured.length > 0 && (
-        <div>
-          <h2>Configured</h2>
-          {configured.map(client => (
-            <PlantConfigForm key={client.id} {...client} />
-          ))}
-        </div>
+        <ClientList
+          title="Configured"
+          clients={configured}
+          setClients={setClients}
+          onPost={onPost}
+        />
       )}
     </Container>
   );
